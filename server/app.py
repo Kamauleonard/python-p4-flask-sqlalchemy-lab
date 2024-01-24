71cbdb96bc30fb96f5cd0da7b1d6ce001f1d6d4d
Kamauleonard <leonardmwangi2012@gmail.com>
2024-01-24 15:48:10 +0300

    Initial Commit

--- server/app.py
+++ server/app.py
@@ -13,21 +13,86 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/')
+@app.route('/') 
 def home():
     return '<h1>Zoo app</h1>'
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal =  Animal.query.filter(Animal.id == id).first()
+    
+    
+    if animal:
+       
+        response_body = f'''<ul>Id: {animal.id}</ul>
+                    <ul>Name: {animal.name}</ul>
+                     <ul>Species: {animal.species}</ul>
+                    <ul>Zookeeper: {animal.zookeeper.name}</ul>
+                     <ul>Enclosure: {animal.enclosure.environment}
+                     </ul>
+                    '''
+
+        
+        response = make_response(response_body, 200)
+        
+
+    else:
+        response_body = '<h1> Not Found<h1>'
+        response = make_response(response_body, 404)
+
+    return response
+
+
+
+  
+
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+    if zookeeper:
+        response_body = f'''<ul>Id: {zookeeper.id}</ul>
+                            <ul>Name: {zookeeper.name}</ul>
+                            <ul>Birthday: {zookeeper.birthday}</ul>
+                            
+                            '''
+        
+        for animal in zookeeper.animals:
+            response_body += f'<ul>Animal: {animal.name}</ul>'
+
+        response_body += '</ul>'
+        
+        response = make_response(response_body, 200)
+    else:
+        response_body = '<h1>Zookeeper not found</h1>'
+
+        response = make_response(response_body, 404)
+
+    return response
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+    
+    if enclosure:
+        response_body = f''' <ul>Id: {enclosure.id}</ul>
+                            <ul>Environment: {enclosure.environment}</ul>
+                            <ul>Open to Visitors: {enclosure.open_to_visitors}</ul>
+                          
+                           
+                            '''
+        
+        for animal in enclosure.animals:
+            response_body += f'<ul>Animal: {animal.name}</ul>'
+
+        response_body += '</ul>'
+        
+        response = make_response(response_body, 200)
+    else:
+        response_body = '<h1>Not Foun<h1>'
+        response = make_response(response_body, 404)
+
+    return response
 
 
 if __name__ == '__main__':
